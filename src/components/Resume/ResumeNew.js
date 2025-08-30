ea9bde084542f4ecf7f3c0c2aaf7844a11ca30e5
yashvinthan <yashvinthanmoorthy@gmail.com>
2025-08-30 20:56:21 +0530

    resume update

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/yashvinthan.pdf";
+import pdf from "../../Assets/../Assets/Yashvinthan_Moorthy_Resume(2025).pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
