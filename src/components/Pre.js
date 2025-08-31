76fdbd40635a6b902585d84c3a2135e0dfdeb624
yashvinthan <yashvinthanmoorthy@gmail.com>
2025-08-31 21:32:20 +0530

    theme update

--- src/components/Pre.js
+++ src/components/Pre.js
@@ -1,6 +1,15 @@
 import React from "react";
+import "../App.css"; // corrected path
+import preloaderSvg from "../Assets/pre.svg"; // corrected path
+
 function Pre(props) {
-  return <div id={props.load ? "preloader" : "preloader-none"}></div>;
+  return (
+    <div id={props.load ? "preloader" : "preloader-none"}>
+      {props.load && (
+        <img src={preloaderSvg} alt="Loading..." className="preloader-icon" />
+      )}
+    </div>
+  );
 }
 
 export default Pre;
